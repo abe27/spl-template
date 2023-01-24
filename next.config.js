dcd3dc79f73875fb70e2805b0388615c61c85b9c
Taweechai Yuenyang <krumii.it@gmail.com>
2023-01-24 16:06:41 +0700

    first init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  swcMinify: true,
+  env: {
+    APP_NAME: process.env.APP_NAME,
+    APP_DESCRIPTION: process.env.APP_DESCRIPTION,
+    API_PUBLISH: process.env.API_HOST,
+    API_HOST: `${process.env.API_HOST}/api/v1`,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
